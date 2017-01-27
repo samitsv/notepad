2aaada23ba3a1dcc63971781941348539b0f24c7
Samit Vaidya <samitsv@gmail.com>
2017-01-27 12:52:03 +0100

    display note content only when clicked on a note

--- controllers/contentController.js
+++ controllers/contentController.js
@@ -1,11 +1,28 @@
 var app = angular.module('notepad');
 
 app.controller('contentController',['$scope', function($scope) {
+    //display note only when clicked
+    $scope.displayContent = false;
     $scope.noteNumber = 1;
     $scope.notes = [{noteNumber:  $scope.noteNumber}];
 
+    //check for the local storage support
+    if (typeof(Storage) !== "undefined") {
+        // Code for localStorage/sessionStorage.
+    } else {
+        // Sorry! No Web Storage support..
+    }
     $scope.addNewNote = function() {
         $scope.noteNumber++;
-         $scope.notes.push({noteNumber: $scope.noteNumber});
+        $scope.notes.push({noteNumber: $scope.noteNumber});
+    }
+
+    $scope.displayNote = function(noteIndex) {
+        console.log(noteIndex);
+        $scope.displayContent = true;
+    }
+
+    $scope.saveNote = function() {
+
     }
 }]);
