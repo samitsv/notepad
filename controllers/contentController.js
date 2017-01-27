e7d86e3340b21c04517c0f3de71f3280ad66195f
Samit Vaidya <samitsv@gmail.com>
2017-01-27 12:30:47 +0100

    content controller created, with add new note function

--- controllers/contentController.js
+++ controllers/contentController.js
@@ -1,2 +1,11 @@
 var app = angular.module('notepad');
 
+app.controller('contentController',['$scope', function($scope) {
+    $scope.noteNumber = 1;
+    $scope.notes = [{noteNumber:  $scope.noteNumber}];
+
+    $scope.addNewNote = function() {
+        $scope.noteNumber++;
+         $scope.notes.push({noteNumber: $scope.noteNumber});
+    }
+}]);
